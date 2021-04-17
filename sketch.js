0576f5548d475f6faaab3c301b32f8a0a2a9f225
Akshaj Rayat <64421667+akshajrayat@users.noreply.github.com>
2021-04-17 16:28:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,36 @@ function preload(){
  happydog =loadImage("happydog.png")
 }
 function setup() {
+  database = firebase.database();
+  console.log(database);
   createCanvas(500,500); 
-  dog = new createSprite(400, 200, 50, 50);
+  dog1 = createSprite(400, 200, 50, 50);
+  dog1.addImage(dog);
+  foodStock=database.ref('food')
+  foodStock.on("value",readStock);
 }
 
 function draw() {
   background(46, 139, 87); 
+  dog1.addImage(dog)
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog1.addImage(happydog);
+    
+  }
+
   drawSprites();
 }
+function readStock(data){
+   foodS=data.val();
+}
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1
+  }
+  }
+database.ref('food').update({
+  Food:x
+})
